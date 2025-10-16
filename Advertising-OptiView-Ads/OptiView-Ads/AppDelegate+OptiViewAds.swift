754bc72c688e83ac276af387b10d7ea69cfdb946
Ceyhun Ozgoc <ceyhun.ozgoc@theoplayer.com>
2025-10-16 15:47:22 +0200

    Add OptiView Ads sample

---
+++ Advertising-OptiView-Ads/OptiView-Ads/AppDelegate+OptiViewAds.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate+OptiViewAds.swift
+//  OptiView-Ads
+//
+//  Copyright © 2025 Dolby OptiView. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegateOptiViewAds: AppDelegate {
+    override class var ROOT_VC_CLASS: UIViewController.Type { PlayerViewControllerOptiViewAds.self }
+}
